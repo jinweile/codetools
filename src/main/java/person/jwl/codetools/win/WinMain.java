fedbdf58d4ca1aae1e8d934ffee4a094a3e4f583
金为乐 <wl.jin@jiangtai.com>
2014-09-11 23:39:37 +0800

    create

---
+++ src/main/java/person/jwl/codetools/win/WinMain.java
@@ -0,0 +1,5 @@
+package person.jwl.codetools.win;
+
+public class WinMain {
+
+}
---
+++ src/test/java/person/jwl/codetools/win/test/WinMainTest.java
@@ -0,0 +1,10 @@
+package person.jwl.codetools.win.test;
+
+public class WinMainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
